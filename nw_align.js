2a1c795015dc76bf97bb617a4d385adb87a7056f
Brian Naughton <briannaughton@gmail.com>
2017-10-15 12:47:33 -0700

    js now working correctly

--- nw_align.js
+++ nw_align.js
@@ -8,6 +8,7 @@ const
 // from string to uint8 list/array and back again
 const ntlist = ["","A","C","G","T","-"]
 const ntdict = {"":0, "A":1, "C":2, "G":3, "T":4, "-":5}
+const eps = 0.0000001;
 
 function _convert_dna(dnastr) {
     let dnalist = dnastr.split('');
@@ -40,63 +41,59 @@ function repeat(dimensions, val) {
 
 function global_align(seqi, seqj, gap, match, mismatch) {
   if (gap===undefined) gap = -1;
-  if (match===undefined) match = -1;
+  if (match===undefined) match = 1;
   if (mismatch===undefined) mismatch = -1;
-  
+
   let arrj = _convert_dna(seqj)
   let arri = _convert_dna(seqi)
 
   let max_j = arrj.length;
   let max_i = arri.length;
-  
-  console.log("max "+max_i+" "+max_j);
-  var score = repeat([max_i+1, max_j+1], -1);
-  var point = repeat([max_i+1, max_j+1], -1);
-  
+
+  var score = repeat([max_i+1, max_j+1], 0);
+  var point = repeat([max_i+1, max_j+1], 0);
+
   score[0][0] = 0.0;
-  
+
   for (var j=1; j<max_j+1; j++) {
     point[0][j] = LF;
     score[0][j] = gap * j;
   }
-  
+
   for (var i=1; i<max_i+1; i++) {
     point[i][0] = UP;
     score[i][0] = gap * i;
   }
-  
+
   point[0][0] = NO;
 
-  console.log("point\n", point);
-  console.log(score);
-  
-  var 
-    ci = 0, 
+  var
+    ci = 0,
     cj = 0;
-  
+
   for (var i=1; i<max_i+1; i++) {
     ci = arri[i-1];
     for (var j=1; j<max_j+1; j++) {
       cj = arrj[j-1];
-      
+
       if (ci == cj) {
         matchscore = match;
       }
       else {
         matchscore = mismatch;
       }
-      
+
       dg_score = score[i-1][j-1] + matchscore;
       up_score = score[i-1][j] + gap;
       lf_score = score[i][j-1] + gap;
-      
-      if (dg_score >= up_score && dg_score >= lf_score) {
+
+      if (dg_score >= up_score-eps && dg_score >= lf_score-eps) {
         score[i][j] = dg_score;
         point[i][j] = DG;
       }
-      else if (lf_score >= dg_score && lf_score >= up_score) {
+      else if (lf_score >= dg_score-eps && lf_score >= up_score-eps) {
         score[i][j] = lf_score;
-        score[i][j] = LF;
+        point[i][j] = LF;
       }
       else {
         score[i][j] = up_score;
@@ -105,7 +102,7 @@ function global_align(seqi, seqj, gap, match, mismatch) {
     }
   }
 
-  var 
+  var
     align_i = repeat(max_i + max_j, 0),
     align_j = repeat(max_i + max_j, 0),
     i = max_i,
@@ -117,7 +114,7 @@ function global_align(seqi, seqj, gap, match, mismatch) {
   while (i > 0 || j > 0) {
     p = point[i][j];
     s = score[i][j];
-    
+
     if (p == DG) {
       align_j[nij] = arrj[j-1];
       align_i[nij] = arri[i-1];
@@ -139,21 +136,26 @@ function global_align(seqi, seqj, gap, match, mismatch) {
       break
     }
     nij += 1;
-  }  
+  }
 
   return {"align_i":_unconvert_dna(align_i), "align_j":_unconvert_dna(align_j)}
 
 }
 
+function reverse_str(astr) {
+  return astr.split("").reverse().join("");
+}
+
 function nw_align(seqi, seqj) {
   const x = 1;
-  console.log(seqi+" "+seqj);
+  //console.log(seqi+" "+seqj);
   var dnai = _convert_dna(seqi);
   var dnaj = _convert_dna(seqj);
 
   var al = global_align(seqi, seqj);
-  console.log(al.align_i+"\n"+al.align_j);
+  return reverse_str(al.align_i)+"\n"+reverse_str(al.align_j);
 }
 
 // node nw_align.js ACGT TGCA
-nw_align(process.argv[2], process.argv[3]);
+var alstr = nw_align(process.argv[2], process.argv[3]);
+console.log(alstr);
